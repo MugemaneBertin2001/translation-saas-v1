0b7561f3171b9cbdec6accdbf2437121784f3ac3
wayneleon1 <rurangwaleon10@gmail.com>
2024-02-05 21:47:53 +0200

    pricing pages

--- components/ui/PageTrunk.tsx
+++ components/ui/PageTrunk.tsx
@@ -9,16 +9,15 @@ const PageTrunk: React.FC<ParentComponentProps> = ({ children, title }) => {
   return (
     <div>
       <div className=" min-h-screen flex items-center justify-center text-dark mt-10">
-        <div className="bg-white p-8 rounded-lg">
-          <p className="text-3xl  mb-4 text-center text-black font-bold">
+        <div className="bg-white p-8 rounded-lg dark:bg-transparent">
+          <p className="text-3xl  mb-4 text-center text-black font-bold dark:text-white">
             {title}
           </p>
-          <div className="grid grid-cols-1 md:grid-cols-3 self-center item-center">
+          <div className="grid grid-cols-1 md:grid-cols-3 self-center item-center gap-4">
             {children}
           </div>
         </div>
       </div>
-      Hi, Bena Ndagaano, I am not yet invited to Slack and i didn't receive any kind of email inviting me, help
     </div>
   );
 };
