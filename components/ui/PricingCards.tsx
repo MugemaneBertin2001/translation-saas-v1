0945c57525f3ca2f5f3602e997d354ab4978aa62
wayneleon1 <rurangwaleon10@gmail.com>
2024-02-08 10:16:51 +0200

    fetch tiers

--- components/ui/PricingCards.tsx
+++ components/ui/PricingCards.tsx
@@ -1,3 +1,4 @@
+import { CheckIcon } from "lucide-react";
 import React from "react";
 export const tiers = [
   {
@@ -57,8 +58,47 @@ function PricingCards({ redirect }: { redirect: boolean }) {
             key={tier.id}
             className="flex flex-col justify-between rounded-3xl bg-white p-8 shadow-xl ring-1 ring-gray-900/10 sm:p-10"
           >
-
-            
+            <div>
+              <h3
+                id={tier.id + tier.name}
+                className="text-base font-semibold leading-7 text-indigo-600"
+              >
+                {tier.name}
+              </h3>
+              <div className="mt-4 flex items-baseline gap-x-2s">
+                {tier.priceMonthly ? (
+                  <>
+                    <span className="text-5l font-bold tracking-tight text-gray-900">
+                      {tier.priceMonthly}
+                    </span>
+                    <span className="text-base font-semibold leading-7 text-gray-600">
+                      /month
+                    </span>
+                  </>
+                ) : (
+                  <span className="text-5xl font-bold tracking-tight text-gray-900">
+                    Free
+                  </span>
+                )}
+              </div>
+              <p className="mt-6 text-base leading-7 text-gray-600">
+                {tier.description}
+              </p>
+              <ul
+                role="list"
+                className="mt-10 space-y-4 text-sm leading-6 text-gray-600"
+              >
+                {tier.features.map((feature) => (
+                  <li key={feature} className="flex gap-x-3">
+                    <CheckIcon
+                      className="h-6 w-5 flex-none text-indigo-600"
+                      aria-hidden="true"
+                    />
+                    {feature}
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         ))}
       </div>
